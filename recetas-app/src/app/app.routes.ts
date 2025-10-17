76610ae214c909c66a58ff01bca7b4b4ebb9bf6e
Nestor J Solera <59302496+NessDisk@users.noreply.github.com>
2025-10-17 08:22:30 -0500

    feat: pages and router
    - Se crearon las pages  de Index, crearRecetas y detailsReceta.

--- recetas-app/src/app/app.routes.ts
+++ recetas-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { CrearRecetaComponent } from './pages/crear-receta/crear-receta.component';
+import { DetalleRecetaComponent } from './pages/detalle-receta/detalle-receta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'crear', component: CrearRecetaComponent },
+  { path: 'receta/:id', component: DetalleRecetaComponent },
+                              ];
---
+++ recetas-app/src/app/pages/detalle-receta/detalle-receta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-receta',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalle-receta.component.html',
+  styleUrl: './detalle-receta.component.scss'
+})
+export class DetalleRecetaComponent {
+
+}
